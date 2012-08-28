6e2cdeb8cc20c630623f25733377a5b31e1152f6
Dmitry Pejemsky <pezhik@gmail.com>
2012-08-28 16:59:31 +0400

    Initial release

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VulnMan::Application.config.secret_token = '2cbb8fa3b27c4296fb17323395851c184b3735dcacbdcca3c238d49d0bd62d77814b927425422b0d3b532c5eb12e0c1772a5a0d09f2d5f5267e999f5dceb03d5'
